f3aa1943d40ce81e3d9ff2911c09693edd7c2137
bogdinamitawhoami <bogdifromsalem@yahoo.com>
2022-08-30 10:09:50 +0200

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,35 @@
+<template>
+  <div id="app">
+    <h1>IronContacts</h1>
+    <div class="actor-list" v-for="actor in firstFive" :key="actor.id">
+      <table class="table">
+        <thead></thead>
+        <tbody>
+          <tr>
+            <td>{{contact.name}}</td>
+            <td>{{contact.popularity}}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</template>
+
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
+import contacts from "./contacts.json";
+import { toRefs, reactive, defineComponent } from "vue";
 
-<template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
-</template>
+export default defineComponent({
+  name: "App",
+  setup: () => {
+    const actors = contacts;
+    const firstFive = reactive(contacts.slice(0, 5));
+    return firstFive;
+  },
+});
+</script>
 
 <style>
 #app {
